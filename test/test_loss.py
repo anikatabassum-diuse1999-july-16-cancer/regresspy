01f02aad8bbf2752f29cb0053e8591be9f3e953c
unknown <anika20diuse@gmail.com>
2021-08-18 21:33:56 +0600

    file added sucessfully

---
+++ test/test_loss.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from regresspy.loss import mae, sse, mse, rmse
+
+
+def test_mae():
+    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_mae = 1.0
+    assert mae(dummy_predictions, dummy_labels) == dummy_mae
+
+
+def test_sse():
+    dummy_prediction = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_sse = 4.0
+    assert sse(dummy_prediction, dummy_labels) == dummy_sse
+
+def test_mse():
+    dummy_prediction = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_mse = 1.0
+    assert mse(dummy_prediction, dummy_labels) == dummy_mse
+
+def test_rmse():
+    dummy_prediction = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_rmse = 1.0
+    assert rmse(dummy_prediction, dummy_labels) == dummy_rmse
